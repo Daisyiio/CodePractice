84a10bec552687fd0a4444a6f3bf59c79d2d5615
Daisy <554091877@qq.com>
2025-01-29 15:58:29 +0800

    循环转递归

---
+++ 循环转递归.js
@@ -0,0 +1,50 @@
+const arr = [1, 2, 3, 4, 5, 6, 7]
+//实现求和 不用循环 不用数组工具函数
+
+function sum(arr, i) {
+  if (i >= arr.length) return false
+  return arr[i] + sum(arr, i + 1)
+}
+
+let all = sum(arr, 0)
+
+console.log(all, 'all')
+
+// for (初始代码; 条件代码; 循环代码) {
+//   //循环体
+// }
+
+// function m() {
+//   初始代码
+//   function _m() {
+//     if (!条件代码) {
+//       return
+//     }
+//     循环体
+//     循环代码
+//     _m()
+//   }
+//   _m()
+// }
+
+let res = 0
+for (let i = 0; i < arr.length; i++) {
+  res += arr[i]
+}
+
+function m() {
+  let res = 0
+  let i = 0
+  function _m() {
+    if (i >= arr.length) {
+      return
+    }
+    res += arr[i]
+    i++
+    _m()
+  }
+  _m()
+  console.log(res)
+}
+
+m()
