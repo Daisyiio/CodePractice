7b358845dcab1e7442e91faa1b5a894d33613f69
Daisy <554091877@qq.com>
2025-02-01 12:38:42 +0800

    &&和||的规则

---
+++ 关于&&和丨丨.js
@@ -0,0 +1,22 @@
+// 返回结果为 能确定结果的最后一个
+console.log(2&& 4)  //4
+console.log(2 ||4) //2
+
+
+
+console.log(1<2 && 4>5) //false
+console.log(1<2|| 4>5)  //true
+
+
+
+
+
+// //假
+// false
+// null
+// undefined
+// ''
+// 0
+// NaN
+//真
+// 其他
