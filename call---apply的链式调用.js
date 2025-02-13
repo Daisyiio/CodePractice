cdece8820f87be4fc57f83798fc1304609b10edf
Daisy <554091877@qq.com>
2025-02-13 22:40:04 +0800

    call 链式调用

---
+++ call---apply的链式调用.js
@@ -0,0 +1,7 @@
+const r = console.log.call.call.call.call.call.call.call.call.call.call.call.call.call.call.call.apply((a)=>a,[1,2])
+
+
+
+// const r= Function.prototype.call.apply((a)=>a,[1,2])
+
+console.log(r)  //2
