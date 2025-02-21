7bcfc61dde6163b675dfd00a665d25403532b7a2
邹钰杰 <554091877@qq.com>
2025-02-21 17:41:02 +0800

    update

---
+++ 手写promise.js
@@ -0,0 +1,53 @@
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+class MyPromise {
+  #state = PENDING
+  #value = null
+  #handler = undefined
+  #handlers = []
+  constructor(exceutor) {
+    const resolve = (val) => {
+      this.setState(val, FULFILLED)
+    }
+    const reject = (reason) => {
+      this.setState(reason, REJECTED)
+    }
+    try {
+      exceutor(resolve, reject)
+    } catch (err) {
+      reject(err)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    return new Promise((resolve, reject) => {
+      //pending
+      this.#handlers.push(() => {
+        if (this.#state === FULFILLED) {
+          onFulfilled(this.#value)
+        } else if (this.#state === REJECTED) {
+          onRejected(this.#value)
+        }
+      })
+        this.#runTask()
+    })
+  }
+  #runTask() {
+    if (this.#state !== PENDING) {
+      this.#handlers.forEach((cb) => cb())
+      this.#handlers = []
+    }
+  }
+  setState(state, value) {
+    if (this.#state !== PENDING) return
+    this.#state = state
+    this.#value = value
+    this.#runTask()
+  }
+}
+
+const p = new MyPromise((resolve, reject) => {
+  resolve('success')
+})
